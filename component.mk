8d3898481bf58731ded37dcde5cff13d997c2206
epictrave <flacks12@gmail.com>
2020-02-21 17:58:58 +0900

    feat: edit build config

--- component.mk
+++ component.mk
@@ -1,7 +1,9 @@
 COMPONENT_SRCDIRS = . \
-deps/parson
+deps/parson \
+deps/esp32-waterlevel
 
 COMPONENT_ADD_INCLUDEDIRS = . \
-deps/parson
+deps/parson \
+deps/esp32-waterlevel
 
 COMPONENT_EMBED_TXTFILES := deps/global-root-ca/ca.pem
